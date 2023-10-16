e1c6196d0d9fb5eec7b1478cfa7631e705a68dba
k-guest <k.guest.fr@gmail.com>
2023-10-16 16:15:57 +0200

    Finalisation de l'inscription. Fix #2

--- validation.php
+++ validation.php
@@ -1,14 +1,33 @@
 <?php
-require('config/config.php');
-require('model/functions.fn.php');
-session_start();
+    require('config/config.php');
+    require('model/functions.fn.php');
+    session_start();
 
-if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
-	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
+    if (isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) &&
+        !empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
-	// TODO
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
-}else{ 
-	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
-	header('Location: register.php');
-}
+        if (!isUsernameAvailable($db, $username)) {
+            $_SESSION['message'] = 'Erreur : Username indisponible';
+            header('Location: register.php');
+            exit;
+        }
+
+        if (!isEmailAvailable($db, $email)) {
+            $_SESSION['message'] = 'Erreur : Email indisponible';
+            header('Location: register.php');
+            exit;
+        }
+
+        userRegistration($db, $username, $email, $password);
+
+        header('Location: login.php');
+    } else {
+        $_SESSION['message'] = 'Erreur : Formulaire incomplet';
+        header('Location: register.php');
+        exit;
+    }
+?>
