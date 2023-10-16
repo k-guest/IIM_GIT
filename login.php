b0eb18d4fb3425dbf36af7dec56e2479296aa724
k-guest <k.guest.fr@gmail.com>
2023-10-16 14:52:30 +0200

    Finalisation du Login. Fix #1

--- login.php
+++ login.php
@@ -12,18 +12,19 @@ require('model/functions.fn.php');
 ********************************/
 
 if(isset($_POST['email']) && isset($_POST['password'])){
-	if(!empty($_POST['email']) && !empty($_POST['password'])){
+    if(!empty($_POST['email']) && !empty($_POST['password'])){
 
-		// TODO
+        $email = $_POST['email'];
+        $password = $_POST['password'];
 
-		// Force user connection to access dashboard
-		userConnection($db, 'git@initiation.com', 'password');
-		
-		header('Location: dashboard.php');
-
-	}else{
-		$error = 'Champs requis !';
-	}
+        if(userConnection($db, $email, $password)) {
+            header('Location: dashboard.php');
+        } else {
+            $error = 'Mauvais identifiants';
+        }
+    } else {
+        $error = 'Champs requis !';
+    }
 }
 
 /******************************** 
